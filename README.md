4fdc914a2af01c919dd0add6696bbbd910ba7fad
Bernt Anker <xx1487yy>
2019-07-30 20:51:42 +0200

    fixed - did not fetch online script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Install
 ```console
-wget https://github.com/kenguru33/ubuntu-replay/archive/master.zip&&unzip master.zip&&rm master.zip&&cd ubuntu-replay-master&&./replay.sh
+wget -qO- https://raw.githubusercontent.com/kenguru33/ubuntu-replay/master/main.sh | bash -s
 ```
--- shell-environment.sh
+++ shell-environment.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# shellcheck source=lib/package.sh
-source "${dir}/lib/package.sh"
-# shellcheck source=lib/spinner.sh
-source "${dir}/lib/spinner.sh"
+if [[ $ONLINE -eq 1 ]]; then
+    # shellcheck source=lib/package.sh
+    source <(wget -qO- https://raw.githubusercontent.com/kenguru33/ubuntu-replay/develop/lib/package.sh) &>/dev/null
+    # shellcheck source=lib/spinner.sh
+    source <(wget -qO- https://raw.githubusercontent.com/kenguru33/ubuntu-replay/develop/lib/spinner.sh) &>/dev/null
+else
+    dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    # shellcheck source=lib/package.sh
+    source "${dir}/lib/package.sh"
+    # shellcheck source=lib/spinner.sh
+    source "${dir}/lib/spinner.sh"
+fi
 
 setDefaultShell() {
     local defaultShell="${1:-c}"
