e5dfbac05c2481a7a64a7b5dd0704132e0f866bf
Bernt Anker <xx1487yy>
2019-08-03 02:29:17 +0200

    move status of each operation far right

--- lib/spinner.sh
+++ lib/spinner.sh
@@ -8,7 +8,7 @@ nc="\\e[0m"
 spinner(){
 	case $1 in
 	start)
-		declare -i rightMargin="${3:-48}"
+		declare -i rightMargin="${3:-8}"
 		_label="${2:-Doing some work...}"
 		_column=$(( $(tput cols)-${#_label}-rightMargin ))
 		_start_spinner &
